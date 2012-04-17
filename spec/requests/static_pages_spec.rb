0c07d0a601d5449cb0e9df69b6ff0b5e0345b5e0
scott fowler <scott@contextualsystems.com>
2012-04-18 07:08:31 +1000

    [from now] 2012/04/18 07:08:31
    diff --git a/spec/requests/static_pages_spec.rb b/spec/requests/static_pages_spec.rb
    index 532088d..8209d48 100644
    --- a/spec/requests/static_pages_spec.rb
    +++ b/spec/requests/static_pages_spec.rb
    @@ -2,6 +2,8 @@ require 'spec_helper'
    
     describe 'Static pages' do
    
    +  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
    +
       describe 'Home page' do
    
         it 'should have the content "Sample App"' do
    @@ -12,7 +14,7 @@ describe 'Static pages' do
         it 'should have the right title' do
           visit '/static_pages/home'
           page.should have_selector('title',
    -      :text => "Ruby on Rails Tutorial Sample App | Home")
    +      :text => "#{base_title} | Home")
         end
       end
    
    @@ -26,7 +28,7 @@ describe 'Static pages' do
         it 'should have the right title' do
           visit '/static_pages/help'
           page.should have_selector('title',
    -      :text => "Ruby on Rails Tutorial Sample App | Help")
    +      :text => "#{base_title} | Help")
         end
       end
    
    @@ -40,7 +42,7 @@ describe 'Static pages' do
         it 'should have the correct title' do
           visit '/static_pages/about'
           page.should have_selector('title',
    -      :text => "Ruby on Rails Tutorial Sample App | About")
    +      :text => "#{base_title} | About")
         end
       end

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'Static pages' do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe 'Home page' do
     
     it 'should have the content "Sample App"' do
@@ -12,7 +14,7 @@ describe 'Static pages' do
     it 'should have the right title' do
       visit '/static_pages/home'
       page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Home")
+      :text => "#{base_title} | Home")
     end
   end
   
@@ -26,7 +28,7 @@ describe 'Static pages' do
     it 'should have the right title' do
       visit '/static_pages/help'
       page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Help")
+      :text => "#{base_title} | Help")
     end
   end
   
@@ -40,7 +42,7 @@ describe 'Static pages' do
     it 'should have the correct title' do
       visit '/static_pages/about'
       page.should have_selector('title', 
-      :text => "Ruby on Rails Tutorial Sample App | About")              
+      :text => "#{base_title} | About")              
     end
   end
   

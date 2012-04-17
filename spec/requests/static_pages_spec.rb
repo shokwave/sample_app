b9a3c7038a54cf4bd70aa1fe2ac3e980d275c7e6
scott fowler <scott@contextualsystems.com>
2012-04-18 07:05:40 +1000

    [from now] 2012/04/18 07:05:40
    diff --git a/app/controllers/static_pages_controller.rb b/app/controllers/static_pages_controller.rb
    index 953e2d7..50e74cc 100644
    --- a/app/controllers/static_pages_controller.rb
    +++ b/app/controllers/static_pages_controller.rb
    @@ -8,4 +8,7 @@ class StaticPagesController < ApplicationController
       def about
       end
    
    +  def contact
    +  end
    +
     end
    diff --git a/app/views/static_pages/about.html.erb b/app/views/static_pages/about.html.erb
    index 86e8621..5b1ff25 100644
    --- a/app/views/static_pages/about.html.erb
    +++ b/app/views/static_pages/about.html.erb
    @@ -1,5 +1,5 @@
     <% provide :title, 'About' %>
    -<h1>Sample App</h1>
    +<h1>About</h1>
     <p>
     	The <a href="http://railstutorial.org/">Ruby on Rails Tutorial</a>
     	is a project to make a book and screencasts to teach web development
    diff --git a/app/views/static_pages/contact.html.erb b/app/views/static_pages/contact.html.erb
    new file mode 100644
    index 0000000..89c6972
    --- /dev/null
    +++ b/app/views/static_pages/contact.html.erb
    @@ -0,0 +1,5 @@
    +<% provide :title, 'Contact'%>
    +<h1>Contact</h1>
    +<b>
    +	Contact me at shokwave.sf@gmail.com.
    +</b>
    \ No newline at end of file
    diff --git a/app/views/static_pages/help.html.erb b/app/views/static_pages/help.html.erb
    index 8b1d729..0fe62de 100644
    --- a/app/views/static_pages/help.html.erb
    +++ b/app/views/static_pages/help.html.erb
    @@ -1,5 +1,5 @@
     <% provide :title, 'Help'%>
    -<h1>Sample App</h1>
    +<h1>Help</h1>
     <p>
     	Get help on the Ruby on Rails Tutorial at the
     	<a href="http://railstutorial.org/help">Rails Tutorial help page</a>.
    diff --git a/config/routes.rb b/config/routes.rb
    index fec869d..b457cb0 100644
    --- a/config/routes.rb
    +++ b/config/routes.rb
    @@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
       get "static_pages/help"
    
       get "static_pages/about"
    +
    +  get "static_pages/contact"
    
       # The priority is based upon order of creation:
       # first created -> highest priority.
    diff --git a/spec/requests/static_pages_spec.rb b/spec/requests/static_pages_spec.rb
    index f65a13b..532088d 100644
    --- a/spec/requests/static_pages_spec.rb
    +++ b/spec/requests/static_pages_spec.rb
    @@ -6,7 +6,7 @@ describe 'Static pages' do
    
         it 'should have the content "Sample App"' do
           visit '/static_pages/home'
    -      page.should have_content('Sample App')
    +      page.should have_selector('h1', :text => 'Sample App')
         end
    
         it 'should have the right title' do
    @@ -20,7 +20,7 @@ describe 'Static pages' do
    
         it 'should have the content "Help"' do
           visit '/static_pages/help'
    -      page.should have_content('Help')
    +      page.should have_selector('h1', :text => 'Help')
         end
    
         it 'should have the right title' do
    @@ -34,7 +34,7 @@ describe 'Static pages' do
    
         it 'should have the content "About"' do
           visit '/static_pages/about'
    -      page.should have_content('About')
    +      page.should have_selector('h1', :text => 'About')
         end
    
         it 'should have the correct title' do
    @@ -42,8 +42,20 @@ describe 'Static pages' do
           page.should have_selector('title',
           :text => "Ruby on Rails Tutorial Sample App | About")
         end
    -
       end
    
    +  describe 'Contact page' do
    +
    +    it 'should have the content "Contact"' do
    +      visit '/static_pages/contact'
    +      page.should have_selector('h1', :text => 'Contact')
    +    end
    +
    +    it 'should have the correct title' do
    +      visit '/static_pages/contact'
    +      page.should have_selector('title',
    +      :text => 'Ruby on Rails Tutorial Sample App | Contact')
    +    end
    +  end
    
     end
    \ No newline at end of file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe 'Static pages' do
     
     it 'should have the content "Sample App"' do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
     
     it 'should have the right title' do
@@ -20,7 +20,7 @@ describe 'Static pages' do
     
     it 'should have the content "Help"' do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
     
     it 'should have the right title' do
@@ -34,7 +34,7 @@ describe 'Static pages' do
     
     it 'should have the content "About"' do
       visit '/static_pages/about'
-      page.should have_content('About')
+      page.should have_selector('h1', :text => 'About')
     end
     
     it 'should have the correct title' do
@@ -42,8 +42,20 @@ describe 'Static pages' do
       page.should have_selector('title', 
       :text => "Ruby on Rails Tutorial Sample App | About")              
     end
-  
   end
   
+  describe 'Contact page' do
+    
+    it 'should have the content "Contact"' do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    
+    it 'should have the correct title' do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+      :text => 'Ruby on Rails Tutorial Sample App | Contact')
+    end
+  end
   
 end
